0e3750defc12d473db3c401fe166e1287000ee79
Andtit4 <lirsitogo2021@gmail.com>
2025-04-13 15:04:12 +0000

    en

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "url";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 9000,
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, './src'),
     },
   },
 });
