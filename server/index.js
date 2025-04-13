a5616c7f6b21dd01f868615e65e73aea737ca4c1
Andtit4 <lirsitogo2021@gmail.com>
2025-04-13 14:17:58 +0000

    modified:   .env

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/oddsx')
   .then(() => console.log('Connecté à MongoDB'))
   .catch(err => console.error('Erreur de connexion MongoDB:', err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9990;
 httpServer.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 }); 
